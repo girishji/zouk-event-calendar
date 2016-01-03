6b6a9df069f717b6d05b6d7e88e24e2097e57b68
girish palya <girishji@gmail.com>
2016-01-03 13:47:10 -0800

    message

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="stylesheets/main.css" />
 </head>
 
 <body>
