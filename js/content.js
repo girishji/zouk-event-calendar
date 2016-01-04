624fda52395b2a847972c0cf733120d20909e97e
girish palya <girishji@gmail.com>
2016-01-03 16:39:47 -0800

    message

--- js/content.js
+++ js/content.js
@@ -12,7 +12,8 @@ $(document).ready(function() {
 // directly after the opening <body> tag on each page you want to load it:
 window.fbAsyncInit = function() {
     FB.init({
-        appId      : "<?php echo getenv('FACEBOOK_APP_ID'); ?>", //set from server side
+        //appId      : <?php echo getenv('FACEBOOK_APP_ID'); ?>, //set from server side
+        appId      : 1263842110297634,
         cookie     : true,
         xfbml      : true,
         version    : 'v2.5'
