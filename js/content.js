423bf90a63f128734ea2a0876ce885c6d28c7afe
girish palya <girishji@gmail.com>
2016-01-03 19:24:50 -0800

    message

--- js/content.js
+++ js/content.js
@@ -70,6 +70,7 @@ function buildContent(accessToken) {
                    console.log('success girish ' + response.length);
                    //alert('success girish');
                    for (var i = 0; i < response.length; i++) {
+                       console.log(Object.getOwnPropertyNames(response[i]));
                        if (response[i].hasOwnProperty(body)) {
                            var body = response[i].body;
                            if (body.hasOwnProperty(data)) {
