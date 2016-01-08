26073cb34c37ec25c5d66a33b8ed0fd4da52316c
girish palya <girishji@gmail.com>
2016-01-07 23:36:23 -0800

    message

--- js/content.js
+++ js/content.js
@@ -196,7 +196,7 @@ function buildContent(accessToken) {
         $('#progressBar').hide();
         $('#contentNav').show();
         $('#evTableContent').replaceWith(str);
-        $('#totalEvents').replaceWith(events.length + ' events');
+        $('#totalEvents').replaceWith('<span class="badge">' + events.length + '</span>' + ' events');
         $('#mainContent').show();
         // console.log(str);
         //document.getElementById("z_content").innerHTML = str;
