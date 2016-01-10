5fecb32f143c34270dc4ca212184649bdb7c27ac
girish palya <girishji@gmail.com>
2016-01-10 11:34:27 -0800

    message

--- js/content.js
+++ js/content.js
@@ -664,6 +664,7 @@ function showMap() {
         $('#evTableHeader').hide();
         $("#evTableContent").hide().html(str).fadeIn('fast');
         $('#mainContent').show();
+        $('#map').show();
 
         var myLatLng = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
