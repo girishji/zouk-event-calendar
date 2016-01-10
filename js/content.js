992b8cc4a1fda7ab013fdceeb89eccab4a01bc58
girish palya <girishji@gmail.com>
2016-01-09 20:10:43 -0800

    message

--- js/content.js
+++ js/content.js
@@ -55,6 +55,15 @@ $(document).ready(function() {
             }
         });
     });
+
+    // Modal for top festivals
+    $('#festivalsModal').on('show.bs.modal', function (event) {
+        console.log('here');
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        //modal.find('.modal-title').text('New message to ' + recipient)
+        //modal.find('.modal-body input').val(recipient)
+    })
 });
 
 
