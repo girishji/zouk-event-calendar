885d0d28d1a69a2aae964ca415e89f7bfd33f827
girish palya <girishji@gmail.com>
2016-01-11 09:43:40 -0800

    message

--- js/content.js
+++ js/content.js
@@ -704,7 +704,7 @@ function showFiltered() {
                     <table class="table table-condensed">
                     <thead>
                     <th>Date</th>
-                    <th>Discarded Events</th>
+                    <th>Unrelated Events</th>
                     <th>Attending</th>
                     <tr>
                     </tr>
@@ -1094,19 +1094,25 @@ function getRelativeUrl(url) {
 
 /************************************************************/
 // Havershine formula, in miles
+//function distance(lat1, lon1, lat2, lon2, unit) {
+//    var radlat1 = Math.PI * lat1/180
+//    var radlat2 = Math.PI * lat2/180
+//    var radlon1 = Math.PI * lon1/180
+//    var radlon2 = Math.PI * lon2/180
+//    var theta = lon1-lon2
+//    var radtheta = Math.PI * theta/180
+//    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+//    dist = Math.acos(dist)
+//    dist = dist * 180/Math.PI
+//    dist = dist * 60 * 1.1515
+//    if (unit=="K") { dist = dist * 1.609344 }
+//    if (unit=="N") { dist = dist * 0.8684 }
+//    return dist
+//}
+
 function distance(lat1, lon1, lat2, lon2, unit) {
-    var radlat1 = Math.PI * lat1/180
-    var radlat2 = Math.PI * lat2/180
-    var radlon1 = Math.PI * lon1/180
-    var radlon2 = Math.PI * lon2/180
-    var theta = lon1-lon2
-    var radtheta = Math.PI * theta/180
-    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-    dist = Math.acos(dist)
-    dist = dist * 180/Math.PI
-    dist = dist * 60 * 1.1515
-    if (unit=="K") { dist = dist * 1.609344 }
-    if (unit=="N") { dist = dist * 0.8684 }
-    return dist
+    var p1 = new LatLon(lat1, lon1);
+    var p2 = new LatLon(lat2, lon2);
+    return p1.distanceTo(p2); // d: 969954.166
 }
 
