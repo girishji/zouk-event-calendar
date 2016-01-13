f01acb72a5de35e8d25de03ad4501626fb8fdc93
girish palya <girishji@gmail.com>
2016-01-13 02:04:32 -0800

    message

--- mail.php
+++ mail.php
@@ -7,8 +7,9 @@ $message = new SendGrid\Email();
 $message->addTo('girishji@gmail.com')->
     setFrom('app45746838@heroku.com')->
     setSubject((string) $_GET["subject"])->
-    setText((string) $_GET["message"])->
-    setHtml('<strong>Hello World!</strong>');
+    setText((string) $_GET["message"]);
+
+//    setHtml('<strong>Hello World!</strong>');
 
 try {
     $response = $sendgrid->send($message);
