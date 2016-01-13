616d2675fa5a08262100e7afe097229b36a93b4b
girish palya <girishji@gmail.com>
2016-01-13 01:32:55 -0800

    message

--- mail.php
+++ mail.php
@@ -7,8 +7,17 @@ $message = new SendGrid\Email();
 $message->addTo('girishji@gmail.com')->
     setFrom('zouk-calendar@heroku.com')->
     setSubject((string) $_GET["message-subject"])->
-    setText((string) $_GET["message-text"]);
-$response = $sendgrid->send($message);
+    setText((string) $_GET["message-text"])->
+    setHtml('<strong>Hello World!</strong>');
+
+try {
+    $response = $sendgrid->send($message);
+} catch(\SendGrid\Exception $e) {
+    echo $e->getCode();
+    foreach($e->getErrors() as $er) {
+        echo $er;
+    }
+}
 echo 'done';
-//->    setHtml('<strong>Hello World!</strong>');
+//
 ?>
