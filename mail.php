ae9d5591529cf3780ba2d024a11eab342c76873b
girish palya <girishji@gmail.com>
2016-01-31 17:51:06 -0800

    message

--- mail.php
+++ mail.php
@@ -1,24 +1,40 @@
 <?php
 
-require 'vendor/autoload.php';
-$sendgrid = new SendGrid('app45746838@heroku.com', 'o5tymy0g1404');
-
-$message = new SendGrid\Email();
-$message->addTo('girishji@gmail.com')->
-    setFrom('worldzoukcalendar@heroku.com')->
-    setSubject((string) $_GET["subject"])->
-    setText((string) $_GET["message"]);
-
-//    setHtml('<strong>Hello World!</strong>');
+use \google\appengine\api\mail\Message;
 
 try {
-    $response = $sendgrid->send($message);
-} catch(\SendGrid\Exception $e) {
-    echo $e->getCode();
-    foreach($e->getErrors() as $er) {
-        echo $er;
-    }
+    $message = new Message();
+    $message->setSender("zouk-events@appspot.gserviceaccount.com");
+    $message->setSubject((string) $_GET["subject"])->
+    $message->setText((string) $_GET["message"]);
+    //$message->setTextBody("Hello, world!");
+    $message->addTo("girishji@gmail.com");
+    $message->send();
+} catch (InvalidArgumentException $e) {
+    echo $e;
 }
+
+
+// Following is for Heroku
+// require 'vendor/autoload.php';
+// $sendgrid = new SendGrid('app45746838@heroku.com', 'o5tymy0g1404');
+// 
+// $message = new SendGrid\Email();
+// $message->addTo('girishji@gmail.com')->
+//     setFrom('worldzoukcalendar@heroku.com')->
+//     setSubject((string) $_GET["subject"])->
+//     setText((string) $_GET["message"]);
+// 
+// //    setHtml('<strong>Hello World!</strong>');
+// 
+// try {
+//     $response = $sendgrid->send($message);
+// } catch(\SendGrid\Exception $e) {
+//     echo $e->getCode();
+//     foreach($e->getErrors() as $er) {
+//         echo $er;
+//     }
+// }
 //var_dump($response); // appears on chrome console
 echo 'done';
 //
