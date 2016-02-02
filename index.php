5369fa73e7f2def97577778db1bf6342dbf31f40
girish palya <girishji@gmail.com>
2016-02-01 17:31:42 -0800

    message

--- index.php
+++ index.php
@@ -118,7 +118,7 @@
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary" onclick="sendMessage();">Send</button>
+              <!-- <button type="button" class="btn btn-primary" onclick="sendMessage();">Send</button> -->
             </div>
           </div>
         </div>
