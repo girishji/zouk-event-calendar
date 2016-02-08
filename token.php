947e1b9e8b655202340813610b844d5a9fa17438
girish palya <girishji@gmail.com>
2016-02-07 23:29:07 -0800

    message

--- token.php
+++ token.php
@@ -3,7 +3,7 @@
 // Get FB token and send back DB content
 
 // vendor dir has all the 3rd party php libs; on heroku you can put this in .gitignore since heroku installs all these on its side; 
-//  however google app engine i am not sure
+//  same thing happens on google app engine
 require('./vendor/autoload.php');
 
 // Store FB token
@@ -23,13 +23,6 @@ $fb = new Facebook\Facebook([
     'app_secret' => $appSecret,
     'default_graph_version' => 'v2.2',
 ]);
-
-if (! $_SESSION['facebook_access_token']) {
-    // User has approved this app through fb login prior to coming here. Get access token from GET
-    // request of ajax
-    // caution: can't use getJavaScriptHelper since this is server side.
-    $_SESSION['facebook_access_token'] = $accessToken;
-}
 
 //To extend an access token, you can make use of the OAuth2Client.
 // OAuth 2.0 client handler
