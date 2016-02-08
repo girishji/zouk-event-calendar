372f7e3089d257fec4de1054b40c3a037507d6d3
girish palya <girishji@gmail.com>
2016-02-07 23:26:26 -0800

    message

--- token.php
+++ token.php
@@ -38,5 +38,7 @@ $oAuth2Client = $fb->getOAuth2Client();
 // Exchanges a short-lived access token for a long-lived one
 $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
 
+echo $longLivedAccessToken;
+
 
 ?>
