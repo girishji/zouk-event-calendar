9449a8344f5d3e8190ff0dc3ec67ebe2e87f2a6a
girish palya <girishji@gmail.com>
2016-02-28 10:37:25 -0800

    message

--- store.php
+++ store.php
@@ -5,6 +5,8 @@ require('./common.php');
 
 $file = (string) $_GET["file"];
 $data = (string) $_GET["data"];
+print_r($data);
+print_r($file);
 storeGCS($data, $bucket, $file);
 
 //echo 'done'; // sent back to ajax caller
