66c27a52efe285583458027764f8be55f41774de
girish palya <girishji@gmail.com>
2016-02-28 10:48:02 -0800

    message

--- store.php
+++ store.php
@@ -3,11 +3,13 @@ require('./vendor/autoload.php');
 require('./config.php');
 require('./common.php');
 
-$file = (string) $_GET["file"];
-$data = (string) $_GET["data"];
-syslog(LOG_DEBUG, print_r($data, TRUE));
-syslog(LOG_DEBUG, print_r($file, TRUE));
-storeGCS($data, $bucket, $file);
+// $file = (string) $_GET["file"];
+// $data = (string) $_GET["data"];
+// syslog(LOG_DEBUG, print_r($data, TRUE));
+// syslog(LOG_DEBUG, print_r($file, TRUE));
+// storeGCS($data, $bucket, $file);
+
+syslog(LOG_DEBUG, print_r((string) $_GET, TRUE));
 
 //echo 'done'; // sent back to ajax caller
 ?>
