bbecbfa413e3f44f558aba5972db8d5066e7d942
girish palya <girishji@gmail.com>
2016-03-21 16:45:09 -0700

    message

--- common.php
+++ common.php
@@ -22,19 +22,19 @@ function getClient() {
     $client->setAccessType("offline");
     $client->useApplicationDefaultCredentials();  // no need to acquire special credentials
 
-    $token = $client->getAccessToken();
-    if (!$token) {
-        syslog(LOG_DEBUG, "girish: access token not present");
-        $token = $client->fetchAccessTokenWithAssertion();
-    }
-    if ($client->isAccessTokenExpired()) {
-        syslog(LOG_DEBUG, "girish: access token expired");
-        $token = $client->fetchAccessTokenWithAssertion();
-        if ($client->isAccessTokenExpired()) {
-            syslog(LOG_DEBUG, "girish: access token still expired");
-            $client->fetchAccessTokenWithRefreshToken($token);
-        }
-    }
+    //$token = $client->getAccessToken();
+    //if (!$token) {
+    //    syslog(LOG_DEBUG, "girish: access token not present");
+    //    $token = $client->fetchAccessTokenWithAssertion();
+    //}
+    //if ($client->isAccessTokenExpired()) {
+    //    syslog(LOG_DEBUG, "girish: access token expired");
+    //    $token = $client->fetchAccessTokenWithAssertion();
+    //    if ($client->isAccessTokenExpired()) {
+    //        syslog(LOG_DEBUG, "girish: access token still expired");
+    //        $client->fetchAccessTokenWithRefreshToken($token);
+    //    }
+    //}
     return $client;
 }
 
