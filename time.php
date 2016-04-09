46bbb08f29bea0e97f0c15df8403af19968e9964
girish palya <girishji@gmail.com>
2016-04-09 08:36:19 -0700

    message

--- time.php
+++ time.php
@@ -5,8 +5,9 @@ require('./common.php');
 
 try {
     $date = new DateTime();
-    $curTime = $date->getTimestamp();
-    echo $curTime;
+    //$curTime = $date->getTimestamp();
+    //echo $curTime;
+    echo $date->format(DateTime::ATOM);
 } catch (Exception $e) {
     syslog(LOG_ERR, $e->getMessage());
     echo "-1";
